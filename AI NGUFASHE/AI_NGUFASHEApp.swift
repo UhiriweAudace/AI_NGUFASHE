fa9013aa287300d36738322ca44b9468e25bddf1
UhiriweAudace <u.audace@gmail.com>
2025-11-18 13:02:44 +0200

    Initial Commit

---
+++ AI NGUFASHE/AI_NGUFASHEApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_NGUFASHEApp.swift
+//  AI NGUFASHE
+//
+//  Created by Audace Uhiriwe on 11/18/25.
+//
+
+import SwiftUI
+
+@main
+struct AI_NGUFASHEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
